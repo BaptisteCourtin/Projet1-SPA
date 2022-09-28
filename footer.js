49a8e12d0805388011b1826adf1ad72b97564112
Baptiste Courtin <bap2910@gmail.com>
2022-09-28 09:24:40 +0200

    header and footer de base

---
+++ footer.js
@@ -0,0 +1,26 @@
+// footer 
+document.write(
+    `
+    <section class="bot-navbar">
+        <a href="index.html" class="logo">D</a>
+        <a href="#">
+            <i class="fa-solid fa-cart-shopping"></i>
+        </a>
+    </section>
+
+    <footer>
+        <div class="icons-applis">
+            <a href="#">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="#">
+                <i class="fa-brands fa-twitter"></i>
+            </a>
+            <a href="">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </div>
+        <p class="copyright">© Baptiste Courtin /  /  /</p>
+    </footer>
+    `
+)
---
+++ header.js
@@ -0,0 +1,24 @@
+// header
+
+document.write(
+    `
+    <header>
+        <div class="nav-flex">
+            <a href="#" class="logo">D</i></a>
+            <button class="mobile-nav-toggle">
+                <i class="fa-solid fa-bars"></i>
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+            <nav class="side-nav">
+                <ul>
+                    <li><a href="#">Nos Soins</a></li>
+                    <li><a href="#">Espace Détente</a></li>
+                    <li><a href="#">Info Pratiques</a></li>
+                    <li><a href="#">Boutique</a></li>
+                </ul>
+            </nav>
+            <a href="#"><i class="fa-solid fa-user"></i></a>
+        </div>
+    </header>
+    `
+)
