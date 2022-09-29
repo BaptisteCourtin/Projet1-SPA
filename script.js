e62e41a93043d3b885ede756337acdfa8803a5ac
Mathieu117 <mathieu.manssens@gmail.com>
2022-09-29 10:02:31 +0200

    ajout header et footer

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// juste pour que ce soit pas vide
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const sideNav = document.querySelector(".side-nav");
+const body = document.querySelector("body");
+
+navToggle.addEventListener('click', () => {
+    sideNav.classList.toggle('opened');
+    body.classList.toggle('opened');
+})
