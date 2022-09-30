9108e3d7b60ce78bc180bc963c2206acc588465c
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2022-09-30 10:07:07 +0200

    ajax working

--- headerscript.js
+++ headerscript.js
@@ -4,6 +4,24 @@ fetch("./header.html")
     })
     .then(data => {
         document.querySelector("header").innerHTML = data;
+    })
+    .then(() => {
+        const navToggle = document.querySelector(".mobile-nav-toggle");
+        const sideNav = document.querySelector(".side-nav");
+        const body = document.querySelector("body");
+
+        navToggle.addEventListener('click', () => {
+            sideNav.classList.toggle('opened');
+            body.classList.toggle('opened');
+        })
+
+        let ajout = document.querySelectorAll(".panier");
+        for (let i = 0; i < ajout.length; i++) {
+            ajout[i].addEventListener('click', function () {
+                ajout[i].innerHTML = "Ajouté !";
+            })
+        }
+
     });
 fetch("./footer.html")
     .then(response => {
