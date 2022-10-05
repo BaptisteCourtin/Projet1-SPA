e9045b893182ab9161fb238c50dbb587b4392e73
Mathieu117 <mathieu.manssens@gmail.com>
2022-10-05 14:05:53 +0200

    input date ok

--- script/headerscript.js
+++ script/headerscript.js
@@ -17,10 +17,10 @@ fetch("../pages/header.html")
 
     // ---
 
-    let ajout = document.querySelector(".panier");
+    let ajout = document.querySelector(".panier1");
+    let ajout2 = document.querySelector(".panier2");
+    let ajout3 = document.querySelector(".panier3");
     const selectDate = (dateSelected) => {
-      //   for (let i = 0; i < ajout.length; i++) {
-      // dateSelected.addEventListener("click", function () {
       if (document.getElementById("choice1").checked) {
         dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
           document.getElementById("choice1").value
@@ -35,8 +35,44 @@ fetch("../pages/header.html")
         }`;
       }
     };
+    const selectDate2 = (dateSelected) => {
+      if (document.getElementById("choice4").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice4").value
+        }`;
+      } else if (document.getElementById("choice5").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice5").value
+        }`;
+      } else if (document.getElementById("choice6").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice6").value
+        }`;
+      }
+    };
+    const selectDate3 = (dateSelected) => {
+      if (document.getElementById("choice7").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice7").value
+        }`;
+      } else if (document.getElementById("choice8").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice8").value
+        }`;
+      } else if (document.getElementById("choice9").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice9").value
+        }`;
+      }
+    };
     ajout.addEventListener("click", function () {
       selectDate(ajout);
+    });
+    ajout2.addEventListener("click", function () {
+      selectDate2(ajout2);
+    });
+    ajout3.addEventListener("click", function () {
+      selectDate3(ajout3);
     });
   });
 
