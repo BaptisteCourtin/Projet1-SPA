9921a613cee9bffa12846a1554df0ee4db378664
Mathieu117 <mathieu.manssens@gmail.com>
2022-10-05 11:46:13 +0200

    Add input radio and function avalaible

--- script/headerscript.js
+++ script/headerscript.js
@@ -1,32 +1,49 @@
 fetch("../pages/header.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.querySelector("header").innerHTML = data;
-    })
-    .then(() => {
-        const navToggle = document.querySelector(".mobile-nav-toggle");
-        const sideNav = document.querySelector(".side-nav");
-        const body = document.querySelector("body");
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    document.querySelector("header").innerHTML = data;
+  })
+  .then(() => {
+    const navToggle = document.querySelector(".mobile-nav-toggle");
+    const sideNav = document.querySelector(".side-nav");
+    const body = document.querySelector("body");
 
-        navToggle.addEventListener('click', () => {
-            sideNav.classList.toggle('opened');
-            body.classList.toggle('opened');
-        })
-
-        let ajout = document.querySelectorAll(".panier");
-        for (let i = 0; i < ajout.length; i++) {
-            ajout[i].addEventListener('click', function () {
-                ajout[i].innerHTML = "Ajouté !";
-            })
-        }
-
+    navToggle.addEventListener("click", () => {
+      sideNav.classList.toggle("opened");
+      body.classList.toggle("opened");
     });
+
+    // ---
+
+    let ajout = document.querySelector(".panier");
+    const selectDate = (dateSelected) => {
+      //   for (let i = 0; i < ajout.length; i++) {
+      // dateSelected.addEventListener("click", function () {
+      if (document.getElementById("choice1").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice1").value
+        }`;
+      } else if (document.getElementById("choice2").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice2").value
+        }`;
+      } else if (document.getElementById("choice3").checked) {
+        dateSelected.innerHTML = `Réservation confirmée pour :</br> ${
+          document.getElementById("choice3").value
+        }`;
+      }
+    };
+    ajout.addEventListener("click", function () {
+      selectDate(ajout);
+    });
+  });
+
 fetch("../pages/footer.html")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        document.querySelector("footer").innerHTML = data;
-    });
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    document.querySelector("footer").innerHTML = data;
+  });
