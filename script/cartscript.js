d8fd521ed194888ac06cdea0a980349ea9a5dac9
Baptiste Courtin <bap2910@gmail.com>
2022-10-06 16:15:37 +0200

    avec storage

--- script/cartscript.js
+++ script/cartscript.js
@@ -1,15 +1,27 @@
-let fullBoutique = document.querySelectorAll(".individuel button");
+const fullBoutique = document.querySelectorAll(".individuel button");
+
+// ---
 
 const adding = (value) => {
+    nb = sessionStorage.getItem(`nb`); // !important
+    if (nb === null) {
+        nb = 0;
+        sessionStorage.setItem(`nb`, nb);
+    }
+
     const ulCart = document.querySelector(".sideCart");
     const newInCart = document.createElement("li");
     newInCart.innerHTML = `${value}`;
     ulCart.appendChild(newInCart);
+
+    sessionStorage.setItem(`item[${nb}]`, value);
+
+    nb++;
+    sessionStorage.setItem(`nb`, nb); // pour ne pas refresh nb et continuer à ajouter au panier
 };
 
 for (let i = 0; i < fullBoutique.length; i++) {
-    fullBoutique[i].addEventListener("click", (event) => {
-        event.preventDefault();
+    fullBoutique[i].addEventListener("click", () => {
         adding(fullBoutique[i].value);
     });
 }
