4fb509957a7b0f9cb1daa1bf9422399364060963
Nathalie  Dune <nathydune@gmail.com>
2022-10-06 15:49:18 +0200

    gras bouton

--- script/boutique.js
+++ script/boutique.js
@@ -1,6 +1,9 @@
 let nvxButton = document.querySelectorAll(".valide");
-
-    for (let i = 0 ; i < nvxButton.length ; i ++){
-        nvxButton[i].addEventListener("click", function (){
-            nvxButton[i].innerHTML= "C'est Ajouté!";
-})};
+   
+        for (let i = 0 ; i < nvxButton.length ; i ++) {
+            
+          nvxButton[i].addEventListener("click", function (){
+            nvxButton[i].innerHTML= "Ajouté!"; 
+            nvxButton[i].style.fontWeight = "bold";
+            })};
+            
